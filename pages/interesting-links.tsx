6711b6e96c9490cc8b4acb03c02e24edfa1d33a0
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-01-10 13:29:15 -0300

    feat: add search bar to navigate through links

--- pages/interesting-links.tsx
+++ pages/interesting-links.tsx
@@ -1,14 +1,76 @@
+import { FormEvent, useRef, useState } from "react";
 import Layout from "../components/Layout";
 
 const InterestingLinks = (): JSX.Element => {
+  const [filteredLinks, setFilteredLinks] = useState(linksArray);
+  const [links] = useState(linksArray);
+
+  const searchValueRef = useRef(null);
+
+  const handleSubmitSearch = (e: FormEvent) => {
+    e.preventDefault();
+    const searchValue = searchValueRef.current.value;
+    const filteredLinks = links.filter((link) => {
+      return (link.title.toLowerCase().includes(searchValue.toLowerCase())) || (link.description.toLowerCase().includes(searchValue.toLowerCase()));
+    });
+    setFilteredLinks(filteredLinks);
+  };
+
   return (
     <Layout>
       <h1 className="text-3xl font-bold">Interesting Links</h1>
       <p className="mt-4 text-gray-600 dark:text-gray-400">
         Here are some interesting links that I&apos;ve found on the internet.
       </p>
+      <form onSubmit={handleSubmitSearch}>
+        <label
+          htmlFor="default-search"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >
+          Search
+        </label>
+        <div
+          className="relative"
+        >
+          <div
+            className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+          >
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              >
+              </path>
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search..."
+            required
+            ref={searchValueRef}
+            onChange={handleSubmitSearch}
+          />
+          <button
+            type="submit"
+            className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Search
+          </button>
+        </div>
+      </form>
       <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-5">
-        {linksArray.sort(
+        {filteredLinks.sort(
           (a, b) => b.dateAdded - a.dateAdded
         ).map((link) => (
           <a
