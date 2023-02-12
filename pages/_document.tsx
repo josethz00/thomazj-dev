1f160073ae1af978cafd2efb790991219b6d5944
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-02-12 20:00:51 -0300

    styles: stylesheet link for latex

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
         <body className="bg-white dark:bg-black text-gray-900 dark:text-white">
           <Main />
           <NextScript />
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossOrigin="anonymous"></link>
         </body>
       </Html>
     );
